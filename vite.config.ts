4eee16bc45e6789e72787eb7c666c28242364708
Jose Maria Garcia <chemagr25@gmail.com>
2023-11-23 15:23:13 -0600

    photo

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     VitePWA({
       workbox: {
+        globPatterns: ['**/*.{png,jpg,jpeg,svg}'],
         runtimeCaching:[{
           urlPattern: ({url}) =>{
             return url.pathname.startsWith('https://compuactual.azurewebsites.net/')
