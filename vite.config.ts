dd04f4d227e8e55fe624cd955dbcce555dd3fbf1
Jose Maria Garcia <chemagr25@gmail.com>
2023-11-23 15:00:35 -0600

    change url cache

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       workbox: {
         runtimeCaching:[{
           urlPattern: ({url}) =>{
-            return url.pathname.startsWith('https://compuactual.vercel.app')
+            return url.pathname.startsWith('https://compuactual.azurewebsites.net/api/')
           },
           handler: 'CacheFirst'as const,
           options:{
