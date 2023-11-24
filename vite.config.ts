2b9a45a8427affc3ee0f97a6d15352650bd6d11c
Jose Maria Garcia <chemagr25@gmail.com>
2023-11-23 21:59:33 -0600

    cache first

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {VitePWA} from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,20 +11,21 @@ export default defineConfig({
     VitePWA({
       workbox: {
         globPatterns: ['**/*.{jpg,jpeg,png,json,html,js,css,svg,woff2,woff,ttf}'],
-        runtimeCaching:[{
-          urlPattern: ({url}) =>{
-            return url.pathname.startsWith('/api')
-          },
-          handler: 'NetworkFirst' as const,
-          options:{
-            cacheName: 'api-cache',
-            cacheableResponse: {
-              statuses: [0,200]
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith('/api')
+            },
+            handler: 'CacheFirst' as const,
+            options: {
+              cacheName: 'api-cache',
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
             }
           }
-        }],
-      },
-      
+        ]
+      }
     })
   ],
   resolve: {
